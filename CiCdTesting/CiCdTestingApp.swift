a2ecc60cdf380e68f0bd8ab560bfd361eb9eae14
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2024-12-16 12:38:33 +0100

    Initial commit

---
+++ CiCdTesting/CiCdTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCdTestingApp.swift
+//  CiCdTesting
+//
+//  Created by New on 16.12.24..
+//
+
+import SwiftUI
+
+@main
+struct CiCdTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
